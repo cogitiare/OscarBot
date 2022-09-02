e9f93761bf4e907ba73a0e5adce6691a5d4f2802
cogitiare <geo.guffey@gmail.com>
2022-09-02 19:37:34 -0400

    added new Discord JS 14 scaffolding

---
+++ src/bot.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const { BOT_TOKEN } = process.env;
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
+
+client.handleEvents();
+client.handleCommands();
+client.login(BOT_TOKEN);
