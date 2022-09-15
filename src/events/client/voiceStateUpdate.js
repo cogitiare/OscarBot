3d76537d2e45c2a9af6fbf3044ac01250cc8dd7e
cogitiare <geo.guffey@gmail.com>
2022-09-15 15:40:41 -0400

    fixed call ping

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,10 +1,17 @@
-const { ALL_CHANNEL_ID } = require("../../../constants");
+const { ALL_CHANNEL_ID, MAIN_CALL_CHANNEL } = require("../../../constants");
 
 module.exports = {
   name: "voiceStateUpdate",
   async execute(oldState, newState, client) {
-    console.log(`old state ${oldState.channel}\nnew state ${newState.channel}`);
-    if (oldState.channel === null && newState.channel !== null) {
+    console.log(
+      `Members in VC: ${newState.member.voice.channel?.members.size}`
+    );
+    if (
+      oldState.channel === null &&
+      newState.channel !== null &&
+      newState.member.voice.channel?.members.size === 1 &&
+      newState.channelId === MAIN_CALL_CHANNEL
+    ) {
       client.channels.cache
         .get(ALL_CHANNEL_ID)
         .send(`@ everyone ${newState.member.user.username} has started a call`);
