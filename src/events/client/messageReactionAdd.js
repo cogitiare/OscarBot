940476ff01d9c95e07bc8fbaf680aa274717bc4d
cogitiare <geo.guffey@gmail.com>
2023-01-25 01:11:43 -0500

    yes

--- src/events/client/messageReactionAdd.js
+++ src/events/client/messageReactionAdd.js
@@ -50,13 +50,13 @@ module.exports = {
 
         // add each image to embed
         // TODO: WHY this not work
-        // attachments.map((attachment) => {
-        //   newEmbeds.push(
-        //     new EmbedBuilder()
-        //       .setURL(messageReaction.message.url)
-        //       .setImage(attachment)
-        //   );
-        // });
+        attachments.map((attachment) => {
+          newEmbeds.push(
+            new EmbedBuilder()
+              .setURL(messageReaction.message.url)
+              .setImage(attachment)
+          );
+        });
       }
 
       client.channels.cache
